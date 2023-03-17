00107ed6000ea500ebd525dd83a8b69918b6e7ce
LuisCorrea07 <97034439+LuisCorrea07@users.noreply.github.com>
2023-03-17 18:31:34 -0300

    Delete JS directory

--- JS/Clase 5 - Objetos/objetos.js
+++
@@ -1,9 +0,0 @@
-function Persona(nombre, edad, calle){
-    this.nombre = nombre;
-    this.edad = edad;
-    this.calle = calle;
-    this.hablar = function() {console.log("HOLA SOY "+ this.nombre)}
-}
-
-const persona1 =new Persona("Luis", 30, "independencia 357");
-console.log(persona1);
